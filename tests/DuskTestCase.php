5364211ca0e2a7e81cb593ef3b716b50b6ca3f1b
CaddyDz <CaddyDz@users.noreply.github.com>
2020-12-27 01:38:32 +0000

    Fix styling

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -6,9 +6,8 @@ namespace Tests;
 
 use Laravel\Dusk\TestCase as BaseTestCase;
 use Facebook\WebDriver\Chrome\ChromeOptions;
-use Facebook\WebDriver\Remote\RemoteWebDriver;
-use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Facebook\WebDriver\Remote\{DesiredCapabilities, RemoteWebDriver};
 
 abstract class DuskTestCase extends BaseTestCase
 {
